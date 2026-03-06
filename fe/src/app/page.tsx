ece7ec599be14de0c0c46801ce7f3b944af3349a
25lp7859 User <25lp7859@pinecones-iMac.local>
2026-03-06 12:15:44 +0800

    room

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/_components/navbar"
+import { CarouselSec } from "@/_components/CarouselSec";
+
 
 export default function Page() {
 return (
 <main>
 <Navbar />
+<CarouselSec />
 <div className="p-6">
 Home page
 </div>
