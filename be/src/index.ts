8a2c90b908cd903c97b54a9930e113c1d1e7213a
4nand1 <anand.khentii@gmail.com>
2026-03-16 12:23:40 +0800

    little change

--- be/src/index.ts
+++ be/src/index.ts
@@ -29,7 +29,7 @@ app.use("/karaoke", KaraokeRouter);
 app.use("/karaoke/:id/rooms", RoomRouter);
 app.use("/karaoke/:id/menu", MenuRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
