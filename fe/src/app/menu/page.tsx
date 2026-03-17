a2acc41e34c1d59ebcf2acdbbf5efa965871e114
Anu <25LP7596@nest.edu.mn>
2026-03-17 11:11:11 +0800

    itemcard

--- fe/src/app/menu/page.tsx
+++ fe/src/app/menu/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
 export default function MenuPage() {
-  return <div>hi</div>;
+  return (
+    <div className="flex flex-col w-full h-full justify-center">
+      <div className="border p-10 text-2xl font-semibold">Menu</div>
+    </div>
+  );
 }
